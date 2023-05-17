030232950b8671936ab7567f72975bb4f46f003c
Oleh Pobyvanets <pobyvanets2005@gmail.com>
2023-05-17 10:45:40 +0300

    Ultimate solution

--- Pr6/Program.cs
+++ Pr6/Program.cs
@@ -1,22 +1,43 @@
-﻿using System.Xml.Serialization;
+﻿using Newtonsoft.Json;
+using System.Text;
+using System.Xml.Serialization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Pr6
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private const string XmlFilePath = @"C:\Users\Lenovo T470p\source\repos\Pr6\Pr6\StudentData.xml";
+        private const string TxtFilePath = @"C:\Users\Lenovo T470p\source\repos\Pr6\Pr6\StudentText.txt";
+        private const int BIRTH_YEAR_PLACEMENT = 5;
+
+        static async Task Main(string[] args)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(Student));
+            Student.OnSuccesfullParsing += Student_OnSuccesfullParsing;
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Student[]));
 
             Student[] students = CreateArrayOfStudents();
 
             Array.Sort(students);
 
-            SerializeAll( serializer, students);
-            WriteAll(students);
+            SerializeAll(serializer, students);
+            WriteDownAll(students);
+            var task = SerializeAllJsonAsync(students, "Json.json");
+
+            PrintStudentsFromXML(serializer);
+            PrintStudentsFromTxt();
+
+            await task;
         }
-        
+
+        private static void Student_OnSuccesfullParsing(object? sender, EventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The object has been parsed succesfully.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
         static Student[] CreateArrayOfStudents()
         {
             Console.WriteLine("Type number of students in array");
@@ -41,10 +62,10 @@ namespace Pr6
             string[] fullName = Console.ReadLine().Trim().Split();
 
             Console.WriteLine("Enter the birth date using \".\" to split the numbers");
-            string[] birthDate = Console.ReadLine().Trim().Split(".");
+            string birthDate = Console.ReadLine();
 
             Console.WriteLine("Enter the abmission date using \".\" to split the numbers");
-            string[] abmissionDate = Console.ReadLine().Trim().Split(".");
+            string abmissionDate = Console.ReadLine();
 
             Student student = new Student(sequenceNumber, fullName, birthDate, abmissionDate);
             return student;
@@ -52,22 +73,55 @@ namespace Pr6
 
         static void SerializeAll(XmlSerializer serializer, Student[] students)
         {
-            string filePath = @"C:\Users\Lenovo T470p\source\repos\Pr6\Pr6\StudentData";
+            string filePath = XmlFilePath;
 
             using (FileStream data = new FileStream(filePath, FileMode.Create))
             {
+                serializer.Serialize(data, students);
+            }
+        }
+
+        async static Task<string> SerializeJsonAsync(object? obj)
+        {
+            return await Task.FromResult(JsonConvert.SerializeObject(obj, Formatting.Indented));
+        }
+
+        async static Task SerializeAllJsonAsync(Student[] students, string filePath)
+        {
+            using (StreamWriter sw = new(File.Create(filePath)))
+            {
                 for (int i = 0; i < students.Length; i++)
                 {
-                    serializer.Serialize(data, students[i]);
+                    var line = await SerializeJsonAsync(students[0]);
+                    await sw.WriteLineAsync(line);
                 }
             }
         }
 
-        static void WriteAll(Student[] students)
+        async static Task<Student> DeserializeJsonAsync(string line)
         {
-            string filePath = @"C:\Users\Lenovo T470p\source\repos\Pr6\Pr6\StudentText";
+            return await Task.FromResult(JsonConvert.DeserializeObject<Student>(line));
+        }
 
-            using (StreamWriter text = new StreamWriter(filePath, true))
+        async static IAsyncEnumerable<Student> DeserializeAllJsonAsync(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException();
+            }
+
+            using (StreamReader sr = new StreamReader(File.Open(filePath, FileMode.Open)))
+            {
+                while (sr.Peek() > -1)
+                {
+                    yield return await DeserializeJsonAsync(await sr.ReadLineAsync());
+                }
+            }
+        }
+
+        static void WriteDownAll(Student[] students)
+        {
+            using (StreamWriter text = new StreamWriter(TxtFilePath, false))
             {
                 for (int i = 0; i < students.Length; i++)
                 {
@@ -76,10 +130,50 @@ namespace Pr6
             }
         }
 
-        static void PrintStudents()
+        static void PrintStudentsFromXML(XmlSerializer serializer)
+        {
+            int birthYear = ReadBirthYear();
+
+            using (FileStream data = new FileStream(XmlFilePath, FileMode.Open))
+            {
+                Student[] students = (Student[])serializer.Deserialize(data);
+
+                foreach (var student in students)
+                {
+                    if (int.Parse(student.BirthDate.Split('.')[2]) == birthYear)
+                    {
+                        Console.WriteLine(student.ToString());
+                    }
+                }
+            }
+        }
+
+        static void PrintStudentsFromTxt()
+        {
+            int birthYear = ReadBirthYear();
+
+            using (StreamReader ourText = new StreamReader(TxtFilePath, Encoding.UTF8))
+            {
+                while (ourText.Peek() != -1)
+                {
+                    string[] line = ourText.ReadLine().Trim().Split(' ', '.');
+
+                    if (int.Parse(line[BIRTH_YEAR_PLACEMENT]) == birthYear)
+                    {
+                        foreach (var item in line)
+                        {
+                            Console.Write(item + " ");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static int ReadBirthYear()
         {
             Console.WriteLine("Enter the year of birth");
             int birthYear = int.Parse(Console.ReadLine());
+            return birthYear;
         }
     }
 }
--- Pr6/Student.cs
+++ Pr6/Student.cs
@@ -10,6 +10,10 @@ namespace Pr6
     [Serializable]
     public struct Student : IComparable<Student>
     {
+        private static List<int> _sequenceNumbers = new List<int>();
+
+        public static event EventHandler? OnSuccesfullParsing;
+
         private int sequenceNumber;
         public int SequenceNumber
         {
@@ -19,19 +23,47 @@ namespace Pr6
 
         public string Surname;
         public string Name;
-        public DateOnly BirthDate;
-        public DateOnly AbmissionDate;
+
+        private DateOnly _birthDate;
+        public string BirthDate
+        {
+            get => $"{_birthDate.Day}.{_birthDate.Month}.{_birthDate.Year}";
+            set
+            {
+                string[] date = value.Split('.');
+                _birthDate = new DateOnly(int.Parse(date[2]), int.Parse(date[1]), int.Parse(date[0]));
+            }
+        }
+
+        private DateOnly _abmissionDate;
+        public string AbmissionDate
+        {
+            get => $"{_abmissionDate.Day}.{_abmissionDate.Month}.{_abmissionDate.Year}";
+            set
+            {
+                string[] date = value.Split('.');
+                _abmissionDate = new DateOnly(int.Parse(date[2]), int.Parse(date[1]), int.Parse(date[0]));
+            }
+        }
 
         public Student() { }
-        public Student(int sequenceNumber, string[] fullName, string[] birthDate, string[] abmissionDate)
+        public Student(int sequenceNumber, string[] fullName, string birthDate, string abmissionDate)
         {
+            if (_sequenceNumbers.Contains(sequenceNumber))
+            {
+                throw new ArgumentException("There is already a student with the same sequence number.");
+            }
+
             SequenceNumber = sequenceNumber;
             Surname = fullName[0];
             Name = fullName[1];
 
-            BirthDate = new DateOnly(int.Parse(birthDate[2]), int.Parse(birthDate[1]), int.Parse(birthDate[0]));
-            
-            AbmissionDate = new DateOnly(int.Parse(abmissionDate[2]), int.Parse(abmissionDate[1]), int.Parse(abmissionDate[0])); ;
+            BirthDate = birthDate;
+            AbmissionDate = abmissionDate;
+
+            _sequenceNumbers.Add(sequenceNumber);
+
+            OnSuccesfullParsing?.Invoke(this, new EventArgs());
         }
 
         public int CompareTo(Student other)
